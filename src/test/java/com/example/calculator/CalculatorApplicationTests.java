f72ae30197ca5555dbcc7eeb7b89387446cbb2c2
Ameachi Onwudiachi <onwudiachiameachi@gmail.com>
2025-10-22 14:09:26 +0100

    Initial commit - Calculator Spring Boot app

--- src/test/java/com/example/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/example/calculator/CalculatorApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.calculator;
 
+import com.example.calculator.Service.CalculatorService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class CalculatorApplicationTests {
+	private final CalculatorService cakculator = new CalculatorService();
 
 	@Test
-	void contextLoads() {
+	public void testSum(){
+		assertEquals(5, cakculator.sum(2, 3));
 	}
-
 }
